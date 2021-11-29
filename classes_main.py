93da89195cddd11d3b180b0c9f4c686ae7900df3
Endrit-S <su.endrit@gmail.com>
2021-11-29 16:08:35 +0100

    First commmit

--- classes_main.py
+++ classes_main.py
@@ -2,6 +2,6 @@
 from classes import Student
 
 student1 = Student("Endrit", "FFI", "2.8", False)
-student2 = Student("Mustermann", "IT", "2.8", True )
+student2 = Student("Max", "IT", "2.8", True )
 
 print(student2.name)
